b5f59f6a950f8403069992591f953a8c620d0251
leonardoanthony.dev <leonardoanthony.dev@gmail.com>
2022-11-18 15:33:30 -0300

    :truck: chore: Alterando posicionamento dos itens

---
+++ Orange Tech Inter/cursoreact/src/components/Header/index.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { Container } from './style'
+
+const Header = () => {
+  return (
+    <>
+        <Container />
+        <h2>aaaaa</h2>
+    </>
+  )
+}
+
+export default Header
--- Orange Tech Inter/cursoreact/src/pages/home/index.jsx
+++ Orange Tech Inter/cursoreact/src/pages/home/index.jsx
@@ -1,14 +1,14 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
-import Button from '../components/Button'
+import React from "react";
+import { Link } from "react-router-dom";
+import Header from "../components/Header";
 
 const Home = () => {
-  return (<>
-    <h1>Home</h1>
-    <Link to="/login">Ir para o Login</Link>
-    <Button title="Teste 1" />
-    <Button variant='secondary' title="Teste 2" />
-  </>)
-}
+    return (
+        <>
+            <Header />
+            <Link to="/login">Ir para o Login</Link>
+        </>
+    );
+};
 
-export default Home
+export default Home;
