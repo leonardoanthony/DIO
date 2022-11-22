39b1c9ed38c1cfa93a3dc0759f51f565155aba10
leonardoanthony.dev <leonardoanthony.dev@gmail.com>
2022-11-22 14:03:43 -0300

    :sparkles: feat: Styled Components: Tela de login

--- Orange Tech Inter/cursoreact/src/pages/login/index.jsx
+++ Orange Tech Inter/cursoreact/src/pages/login/index.jsx
@@ -1,12 +1,47 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React from "react";
+import Button from "../../components/Button";
+import Header from "../../components/Header";
+import {
+    Column,
+    Container,
+    Content,
+    Form,
+    Input,
+    Title,
+    Row,
+} from "./styles";
 
 const Login = () => {
-  return (<>
-    <h1>Login</h1>
-    <Link to="/">Ir para a Home</Link>
-  
-  </>)
-}
+    return (
+        <>
+            <Header />
+            <Container>
+                <Row>
+                    <Column>
+                        <Title>
+                            A plataforma para você aprender com experts, dominar
+                            as principais tecnologias e entrar no mais rápido
+                            nas empresas mais desejadas
+                        </Title>
+                    </Column>
 
-export default Login
+                    <Column>
+                        <Form>
+                            <Title>Faça seu cadastro</Title>
+                            <Content>make the change!</Content>
+                            <Input placeholder="E-mail" type="email"/>
+                            <Input placeholder="Senha" type="password"/>
+                            <Button title="Entrar" variant="secondary" />
+                            <Row>
+                                <Content variant="yellow">Esqueci a senha!</Content>
+                                <Content variant="pink">Cadastre-se</Content>
+                            </Row>
+                        </Form>
+                    </Column>
+                </Row>
+            </Container>
+        </>
+    );
+};
+
+export default Login;
